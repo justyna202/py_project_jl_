905cdb25a692cf2880ce476e27815692237f9865
justyna202 <justyna.latkowska@student.wat.edu.pl>
2026-06-11 21:52:40 +0200

    Add application data

---
+++ notatnik.py
@@ -0,0 +1,55 @@
+LOGIN = "admin"
+HASLO = "admin"
+
+firmy = [
+    {   "nazwa": "Klinika VetMed",
+        "miejscowosc": "Warszawa",
+        "ulica": "Marszałkowska 10",
+        "lat": 52.2297,
+        "lon": 21.0122,},
+    {   "nazwa": "Przychodnia Łapka",
+        "miejscowosc": "Kraków",
+        "ulica": "Długa 5",
+        "lat": 50.0647,
+        "lon": 19.9450,},]
+
+klienci = [
+    {   "imie_nazwisko": "Anna Kowalska",
+        "firma": "Klinika VetMed",
+        "miejscowosc": "Warszawa",
+        "ulica": "Polna 3",
+        "lat": 52.2310,
+        "lon": 21.0200,},
+    {   "imie_nazwisko": "Jan Nowak",
+        "firma": "Przychodnia Łapka",
+        "miejscowosc": "Kraków",
+        "ulica": "Karmelicka 7",
+        "lat": 50.0660,
+        "lon": 19.9360,},]
+
+pracownicy = [
+    {   "imie_nazwisko": "Maria Wesołowska",
+        "firma": "Klinika VetMed",
+        "stanowisko": "lekarz weterynarii",
+        "miejscowosc": "Warszawa",
+        "ulica": "Puławska 1",
+        "lat": 52.2200,
+        "lon": 21.0140,},
+    {   "imie_nazwisko": "Piotr Olszewski",
+        "firma": "Przychodnia Łapka",
+        "stanowisko": "technik weterynarii",
+        "miejscowosc": "Kraków",
+        "ulica": "Floriańska 2",
+        "lat": 50.0620,
+        "lon": 19.9400,},]
+
+zwierzeta = [
+    {   "nazwa": "Burek",
+        "gatunek": "pies",
+        "wlasciciel": "Anna Kowalska",
+        "pracownik": "Maria Wesołowska",},
+    {   "nazwa": "Filemon",
+        "gatunek": "kot",
+        "wlasciciel": "Jan Nowak",
+        "pracownik": "Piotr Olszewski",},
+]
